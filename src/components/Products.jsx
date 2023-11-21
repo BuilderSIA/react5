883911798895dc49c5bf85f036048fe5dd759230
BuilderSIA <lordallplay@gmail.com>
2023-11-22 02:43:30 +0500

    Editni qilayapman

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,17 +3,17 @@ function Products({pname,setPname,price,setPrice,handEditId,allProd,setAllprod,d
     return (
         <div className="prodCont">
             <div className="prodSide">
-                <input type="text" className="forId" value={id} onChange={(e)=>setId(e.target.value)} />
-                <input type="text" className="forPname" value={pname} onChange={(e)=>setPname(e.target.value)} />
-                <input type="text" className="forPrice" value={price} onChange={(e)=>setPrice(e.target.value)} />
-                <button onClick={()=>handProd()}>
+                <input type="text" className="forI" value={id} onChange={(e)=>setId(e.target.value)} placeholder="Enter id" />
+                <input type="text" className="forP" value={pname} onChange={(e)=>setPname(e.target.value)} placeholder="Enter name" />
+                <input type="text" className="forP" value={price} onChange={(e)=>setPrice(e.target.value)} placeholder="Enter price"/>
+                <button onClick={()=>handProd()} className="addBtn">
                     Add product
                 </button>
                 
             </div>
             <ul className="prodList">
                 {allProd.map((data)=>{
-                    return <div className={data.id} key={data.id}>
+                    return <div className='items' key={data.id}>
                         <h2>
                             {data.name}
                         </h2>
